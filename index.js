fd99528c54c3daa3d12b876724f87cbe25ddc110
Alhajideen <salifunurudeen834@gmail.com>
2022-11-24 16:48:33 -0600

    read start.txt file in an asynchronous way

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
+//Blocking-synchronous way
 const textOut = `This is a message about avocados: ${textIn}. \n created on ${Date.now}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File Written');
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
