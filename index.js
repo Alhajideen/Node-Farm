178c3abbcbef0b7d5cd1ca5c7038a40608c67aac
Alhajideen <salifunurudeen834@gmail.com>
2022-11-24 17:20:14 -0600

    run server using http server

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const fs = require('fs');
+const http = require('http');
+
+/////////////////////////////////////
+///////FILES
 
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-
 //Blocking-synchronous way
 // const textOut = `This is a message about avocados: ${textIn}. \n created on ${Date.now}`;
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File Written');
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, 'utf-8', (err) => {
-        console.log('Files written');
-      });
-    });
-  });
+//       fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, 'utf-8', (err) => {
+//         console.log('Files written');
+//       });
+//     });
+//   });
+// });
+// console.log('Reading file...');
+
+/////////////////////////////////////
+///////SERVER
+const PORT = 8080;
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server');
 });
-console.log('Reading file...');
+
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Listening on Port ${PORT}`);
+});
